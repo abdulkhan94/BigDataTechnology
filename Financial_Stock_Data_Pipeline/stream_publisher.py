edac06173fe94d1836d76455f80f7df56493b727
Abdullah Khan <abdullahn@gmail.com>
2020-11-16 20:27:37 -0800

    Finished publisher python script

--- Financial_Stock_Data_Pipeline/stream_publisher.py
+++ Financial_Stock_Data_Pipeline/stream_publisher.py
@@ -7,8 +7,8 @@ import random
 
 #publisher = pubsub.PublisherClient()
 
-#project = "data228"
-#topic = "data228-hw8-in"
+#project = " "
+#topic = " "
 
 #topic_path = publisher.topic_path(project, topic)
 
@@ -19,13 +19,14 @@ with open('./source_data/aapl_streaming_apr.csv', 'r') as csvfile:
     next(spamreader)
     for row in spamreader:
         try:
-            json_dict['date'] = row[0]
-            json_dict['open'] = float(row[1])
-            json_dict['high'] = float(row[2])
-            json_dict['low'] = float(row[3])
-            json_dict['close'] = float(row[4])
-            json_dict['adj_close'] = float(row[5])
-            json_dict['volume'] = int(row[6])
+            json_dict['ticker'] = row[0]
+            json_dict['date'] = row[1]
+            json_dict['open'] = float(row[2])
+            json_dict['high'] = float(row[3])
+            json_dict['low'] = float(row[4])
+            json_dict['close'] = float(row[5])
+            json_dict['adj_close'] = float(row[6])
+            json_dict['volume'] = int(row[7])
             jstr = json.dumps(json_dict)
             #print("Publishing {} to {} at {}...".format(jstr, topic_path, datetime.datetime.now()))
             #publisher.publish(topic_path, bytes(jstr, "utf-8"))
@@ -34,31 +35,3 @@ with open('./source_data/aapl_streaming_apr.csv', 'r') as csvfile:
             sleep(random.randint(1, 3))
         except IndexError:
             break
-
-
-'''
-publisher = pubsub.PublisherClient()
-
-project = "data228"
-topic = "data228-hw8-in"
-
-topic_path = publisher.topic_path(project, topic)
-
-json_dict = {}
-
-with open('hw8_data.csv', 'r') as csvfile:
-    spamreader = csv.reader(csvfile)
-    next(spamreader)
-
-    for row in spamreader:
-        try:
-            json_dict["id"] = int(row[0])
-            json_dict["steps"] = int(row[1])
-            jstr = json.dumps(json_dict)
-            print("Publishing {} to {} at {}...".format(jstr, topic_path, datetime.datetime.now()))
-            publisher.publish(topic_path, bytes(jstr, "utf-8"))
-            json_dict.clear()
-            sleep(5)
-        except IndexError:
-            break
-'''
