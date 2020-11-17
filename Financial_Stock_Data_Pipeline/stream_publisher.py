751eb6fda81a075da880ada1fc976697be098bd5
Abdullah Khan <abdullahn@gmail.com>
2020-11-16 20:21:28 -0800

    Updated writing stream_publisher.py - this now includes other financial data as well

--- Financial_Stock_Data_Pipeline/stream_publisher.py
+++ Financial_Stock_Data_Pipeline/stream_publisher.py
@@ -14,14 +14,18 @@ import random
 
 json_dict = {}
 
-with open('./source_data/aapl_2020.csv', 'r') as csvfile:
+with open('./source_data/aapl_streaming_apr.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile)
     next(spamreader)
     for row in spamreader:
         try:
-            json_dict['ticker'] = row[0]
-            json_dict['date'] = row[1]
-            json_dict['price'] = float(row[2])
+            json_dict['date'] = row[0]
+            json_dict['open'] = float(row[1])
+            json_dict['high'] = float(row[2])
+            json_dict['low'] = float(row[3])
+            json_dict['close'] = float(row[4])
+            json_dict['adj_close'] = float(row[5])
+            json_dict['volume'] = int(row[6])
             jstr = json.dumps(json_dict)
             #print("Publishing {} to {} at {}...".format(jstr, topic_path, datetime.datetime.now()))
             #publisher.publish(topic_path, bytes(jstr, "utf-8"))
